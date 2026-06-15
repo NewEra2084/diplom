55f1fd4929a41bbdd55e6754db538a8a13acdc24
Mihail Filippov <mihail.filippov2006@gmail.com>
2026-06-15 17:59:20 +0400

    secret secret secret

--- frontend/src/shared/config/env.ts
+++ frontend/src/shared/config/env.ts
@@ -1,3 +1,3 @@
-export const BACKEND_API = "http://localhost:8080/api/";
+export const BACKEND_API = "https://scrumify.ru/crm/api";
 export const TELEGRAM = "https://t.me/NewEra2084";
 export const GITHUB = "https://github.com/NewEra2084/diplom";
